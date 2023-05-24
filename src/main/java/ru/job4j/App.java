4e142507d3d3db8c1158c1ada727d4e527b8b357
iiuhenio <iuheniomail@yandex.ru>
2023-05-24 13:53:13 +0300

    Check github threads project.

---
+++ src/main/java/ru/job4j/App.java
@@ -0,0 +1,13 @@
+package main.java.ru.job4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
